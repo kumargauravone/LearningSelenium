d0cf5d716bbaae4213663f4fba2dfc1d0d169f47
DELL <DELL@DESKTOP-91B94BG>
2025-05-18 18:02:16 +0530

    Selenium getTitle program is added.

---
+++ src/main/java/com/LearningSelenium/basics/SeleniumTest1.java
@@ -0,0 +1,21 @@
+package com.LearningSelenium.basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.qameta.allure.Description;
+
+public class SeleniumTest1 {
+	
+	@Test
+	@Description("Verify the title of the givem website")
+	public void launchBrowser() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");	
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
+
+}
